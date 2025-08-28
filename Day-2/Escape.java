61ff8c206249c2ee2c81618f89e3c5f87b1b0e4f
abin0x <mahmudulabin@gmail.com>
2025-08-28 10:44:19 +0600

    Escape Sequence

---
+++ Day-2/Escape.java
@@ -0,0 +1,8 @@
+public class Escape
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!\nMy full name is mahmudul Hasan abin\nnow iam studying HSTU\nMy ID is:2302040");
+        System.out.println("This is a tabbed space:\tHello");
+    }
+}
