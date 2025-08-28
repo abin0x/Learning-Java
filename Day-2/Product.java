6395d4de3ae4c6daa665824d7350b87512956dae
abin0x <mahmudulabin@gmail.com>
2025-08-28 17:53:12 +0600

    Assignement-2

---
+++ Day-2/Product.java
@@ -0,0 +1,18 @@
+// Assignment-2 (Variable and Data type)
+
+class Product {
+    public static void main(String[] args) {
+
+       int id = 101;
+       String title = "iPhone 15";
+       double price = 1895;
+       String description = "Perfect product with best image quality";
+       String category = "Phone";
+
+        System.out.println("Product ID: " + id);
+        System.out.println("Title: " + title);
+        System.out.println("Price: " + price + " euros");
+        System.out.println("Description: " + description);
+        System.out.println("Category: " + category);
+    }
+}
