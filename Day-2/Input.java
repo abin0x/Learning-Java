2df87d785870831a2e2fb0076e4c51d8e4a4b913
abin0x <mahmudulabin@gmail.com>
2025-08-28 18:23:17 +0600

    Take input from user

---
+++ Day-2/Input.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        String text;
+
+        System.out.println("Please Enter Your name: ");
+        text = input.nextLine();
+
+        System.out.println("Enter an integer number: ");
+        number = input.nextInt();
+
+        input.nextLine(); 
+
+        System.out.println("Hello, I am " + text);
+        System.out.println("You entered: " + number);
+        
+        input.close();
+    }
+}
